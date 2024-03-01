d2762c6d29c6589e57668bf27fc086e0b71f00e2
NirmayNandurkar <157727214+NirmayNandurkar@users.noreply.github.com>
2024-03-01 14:36:38 +0530

    Update 23MM01016 Assignment 5 Question 2.c

--- 23MM01016 Assigment 5 Question 2.c
+++ 23MM01016 Assigment 5 Question 2.c
@@ -2,13 +2,17 @@
 void main()
 {
     int num,div,i,c=0;
-    printf("Enter the dividend: ");
+    printf("Enter the dividend:- \n");
     scanf("%d",&num);
-    printf("Enter the divisor: ");
+    printf("Enter the divisor:- \n");
     scanf("%d",&div);
-    for(i=div;i<=num;i=i+div)
+    for(i=div;i <= num;i =i + div)
+    {
     c++;
+    }
     printf("Quotient= %d\n",c);
     printf("Remainder: %d",(num-i+div));
+
+    return 0;
 
 }
