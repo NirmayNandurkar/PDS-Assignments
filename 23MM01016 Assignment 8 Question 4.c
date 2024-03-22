461d82c4a9db6e3ddb5f7dff8a26307113fa46c9
NirmayNandurkar <157727214+NirmayNandurkar@users.noreply.github.com>
2024-03-22 14:59:26 +0530

    Added Question 4

---
+++ 23MM01016 Assignment 8 Question 4.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void printduplicate(int arr[],int n) {
+    int *ptr=arr,i=0,num;
+    while(i <= n)
+    {
+        int check = 0;
+        int d = *(ptr+i);
+        for(int j = 0;j <= n; j++)
+        {
+            if(d == *(ptr+j))
+                check++;
+        }
+        if(check > 1)
+            num=d;
+        i++;   
+    }
+    printf("%d is repeated 2 times",num);
+    
+}
+int main() {
+    int nums[100],n;
+    
+    printf("Enter size of array:- \n");
+    scanf("%d",&n);
+    
+    for(int i=0;i<=n;i++)
+    {
+        printf("Enter element %d of the array:- \n", i + 1);
+        scanf("%d",&nums[i]);
+    }
+       
+    
+    printduplicate(nums,n);
+    return 0;
+}
