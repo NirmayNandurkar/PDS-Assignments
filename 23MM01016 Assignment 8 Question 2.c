911310efef76f3bd4c9ef00c42c0cc40c2da9f7f
NirmayNandurkar <157727214+NirmayNandurkar@users.noreply.github.com>
2024-04-05 13:40:03 +0530

    Updated 23MM01016 Assignment 8 Question 2.c

--- 23MM01016 Assignment 8 Question 2.c
+++ 23MM01016 Assignment 8 Question 2.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-void matrix_multiplication(int mat1[10][10], int rows1, int cols1, int mat2[10][10], int rows2, int cols2, int result[10][10]) {
+void matrix_multiplication(int *mat1, int rows1, int cols1, int *mat2, int rows2, int cols2, int *result) {
     int i, j, k;
 
     for (i = 0; i < rows1; i++) {
         for (j = 0; j < cols2; j++) {
-            result[i][j] = 0;
+            *(result + i * cols2 + j) = 0;
             for (k = 0; k < cols1; k++) {
-                result[i][j] += mat1[i][k] * mat2[k][j];
+                *(result + i * cols2 + j) += *(mat1 + i * cols1 + k) * *(mat2 + k * cols2 + j);
             }
         }
     }
 }
 
-void print_matrix(int mat[10][10], int rows, int cols) {
+void print_matrix(int *mat, int rows, int cols) {
     int i, j;
     for (i = 0; i < rows; i++) {
         for (j = 0; j < cols; j++) {
-            printf("%d\t", mat[i][j]);
+            printf("%d\t", *(mat + i * cols + j));
         }
         printf("\n");
     }
@@ -55,11 +55,11 @@ int main() {
     }
 
    
-    matrix_multiplication(mat1, rows1, cols1, mat2, rows2, cols2, result);
+    matrix_multiplication(&mat1[0][0], rows1, cols1, &mat2[0][0], rows2, cols2, &result[0][0]);
 
     
     printf("Resulting matrix:\n");
-    print_matrix(result, rows1, cols2);
+    print_matrix(&result[0][0], rows1, cols2);
 
     return 0;
 }
